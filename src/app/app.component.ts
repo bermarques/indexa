d1f9a35ab3ef8fa4885db5d7e21ccece9079102e
Bernardo Marques <bernardohmarques@gmail.com>
2024-09-27 06:02:57 -0300

    chore: create component contato

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +15,11 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
+    ContatoComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefhijklmnopqrstuvwxyz';
 }
