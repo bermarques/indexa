7bce2f7cddcccbf04de7ce3a54481dca6546a326
Bernardo Marques <bernardohmarques@gmail.com>
2024-09-28 20:50:02 -0300

    feature: edit contact

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -8,7 +8,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ContatoService } from '../../services/contato.service';
 
 @Component({
@@ -27,9 +27,14 @@ import { ContatoService } from '../../services/contato.service';
 export class FormularioContatoComponent implements OnInit {
   contatoForm!: FormGroup;
 
-  constructor(private contatoService: ContatoService, private router: Router) {}
+  constructor(
+    private contatoService: ContatoService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
   ngOnInit() {
     this.inicializarFormulario();
+    this.carregarContato();
   }
 
   inicializarFormulario() {
@@ -42,9 +47,21 @@ export class FormularioContatoComponent implements OnInit {
       observacoes: new FormControl(''),
     });
   }
+
+  carregarContato() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.contatoService.buscarPorId(parseInt(id)).subscribe((contato) => {
+        this.contatoForm.patchValue(contato);
+      });
+    }
+  }
   salvarContato() {
     const novoContato = this.contatoForm.value;
-    this.contatoService.salvarContato(novoContato).subscribe(() => {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    novoContato.id = id ? parseInt(id) : null;
+
+    this.contatoService.editarOuSalvarContato(novoContato).subscribe(() => {
       this.contatoForm.reset();
       this.router.navigateByUrl('/lista-contatos');
     });
@@ -52,5 +69,6 @@ export class FormularioContatoComponent implements OnInit {
 
   cancelar() {
     this.contatoForm.reset();
+    this.router.navigateByUrl('/lista-contatos');
   }
 }
